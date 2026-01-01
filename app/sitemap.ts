e45bf1b81a6f98919687f355e79be2e23932da9c
Patel Rohan <rohan_fw11_289@masai.school>
2026-01-01 10:30:32 +0000

    added robots and sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,31 @@
+import type { MetadataRoute } from 'next'
+import { BLOGS } from './constants'
+import { generateSlug } from './utils/slug'
+import { Blog } from './types'
+ 
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"
+  
+  const blogEntries = BLOGS.map((blog: Blog) => ({
+    url: `${baseUrl}/${generateSlug(blog.title)}`,
+    lastModified: new Date(blog.date),
+    changeFrequency: 'monthly',
+    priority: 0.8,
+  }));
+ 
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/blog`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.8,
+    },
+    ...blogEntries,
+  ]
+}
